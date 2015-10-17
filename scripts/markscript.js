3982cc78acc185159d33eddef9e999aca183effb
markgarel17 <markgarel17@gmail.com>
2015-10-17 17:03:45 +1100

    added calc

---
+++ scripts/markscript.js
@@ -0,0 +1,33 @@
+/* Mark Faderon */
+
+/* Calculator */
+
+function calculate(operation)
+{
+	var numOne = parseInt($('#numOne').val());
+	var numTwo = parseInt($('#numTwo').val());
+	var result = "";
+	
+	switch (operation)
+	{
+		case "add": result += numOne + numTwo;
+					$('#operation').text("Sum:");
+					break;
+		case "sub": result += numOne - numTwo;
+					$('#operation').text("Difference:");
+					break;
+		case "mul": result += numOne * numTwo;
+					$('#operation').text("Product:");
+					break;
+		case "div": result += numOne / numTwo;
+					$('#operation').text("Quotient:");
+					break;
+	}
+	
+	if(result == "NaN")
+	{
+		result = "Invalid Number!";
+	}
+
+	$("#result").val(result);
+}
