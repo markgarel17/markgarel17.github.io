3dab007612ac1b7fbf457f4c9a484bdc957371dd
markgarel17 <markgarel17@gmail.com>
2015-10-19 07:29:55 +1100

    new page Person

--- scripts/markscript.js
+++ scripts/markscript.js
@@ -34,5 +34,54 @@ function calculate(operation)
 		result = "Invalid Number!";
 	}
 
-	$("#result").val(result);
+	$('#result').val(result);
+}
+
+function displayPersons()
+{
+	var listofPerson = [
+		{Name: "Mark Faderon", Age: 76, Message: "Hell yeah!"},
+		{Name: "Alexander Nikolaevich", Age: 23, Message: "The Great"},
+		{Name: "Spongebob", Age: 11, Message: "Squarepants"},
+		{Name: "Shanks", Age: 37, Message: "Cool!"},
+		{Name: "Mango Vanilla", Age: 123, Message: "Yum"}
+	]
+	
+	$('table#listofPerson').prepend("<tr><th>Name</th><th>Age</th><th>Message</th></tr>");
+	
+	for(i = 0 ; i < listofPerson.length ; i++)
+	{
+		$('table#listofPerson').append("<tr><td>" + listofPerson[i].Name + 
+									"</td><td>" + listofPerson[i].Age +
+									"</td><td>" + listofPerson[i].Message +
+									"</td></tr>");
+	}
+}
+
+function addPFormShow() {
+	$('#addPersonForm').show();
+	$('#addPersonBtn').prop('disabled', true);
+	$('#addPersonBtn').addClass('buttonActive');
+}
+
+function addPFormHide() {
+	$('#addPersonForm').hide();
+	$('#addPersonBtn').prop('disabled', false);
+	$('#addPersonBtn').removeClass('buttonActive');
+}
+
+function addPerson() {
+	
+	var name = $('#pName').val();
+	var age = $('#pAge').val();
+	var message = $('#pMessage').val();
+	
+	
+	$('table#listofPerson').append("<tr><td>" + name + 
+									"</td><td>" + age +
+									"</td><td>" + message +
+									"</td></tr>");
+	$('#addPersonForm').hide();
+	$('#addPersonBtn').prop('disabled', false);
+	$('#addPersonBtn').removeClass('buttonActive');
 }
